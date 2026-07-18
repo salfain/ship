afc648b827e693fa076e31566bf8bd0a1c90beca
salfain <salfain@ship.id>
2026-07-19 00:31:43 +0700

    revert: restore default Android maps renderer

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'dart:async';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
-import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 import 'app.dart';
@@ -14,7 +11,6 @@ import 'core/platform/google_maps_api_loader.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  _configureAndroidGoogleMapsRenderer();
   await initializeDateFormatting('id_ID');
   try {
     await dotenv.load(fileName: '.env');
@@ -24,13 +20,4 @@ Future<void> main() async {
 
   runApp(const ProviderScope(child: ShipMonitoringApp()));
   unawaited(initializeGoogleMapsApi(Env.googleMapsWebApiKey));
-}
-
-void _configureAndroidGoogleMapsRenderer() {
-  if (kIsWeb || defaultTargetPlatform != TargetPlatform.android) return;
-
-  final mapsImplementation = GoogleMapsFlutterPlatform.instance;
-  if (mapsImplementation is GoogleMapsFlutterAndroid) {
-    mapsImplementation.useAndroidViewSurface = true;
-  }
 }
